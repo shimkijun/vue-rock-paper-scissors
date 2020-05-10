525a25742a978e50f57764f96f66ce44ae0274f8
shimkijun <shimkijun@gmail.com>
2020-05-10 20:24:26 +0900

    code refactoring

--- app.js
+++ app.js
@@ -8,21 +8,79 @@ var app = new Vue({
         lifeOfMe:3,
         lifeOfCom:3,
         isSelectable: true,
-        logs:[]
+        logs:[],
+        selects : [
+            { name : '가위' , value : 'scissor'},
+            { name : '바위' , value : 'rock'},
+            { name : '보' , value : 'paper'},
+        ]
+    },
+    computed:{
+        myChoiceImg(){
+            return this.myChoice !== null ? `images/${this.myChoice}.jpg` : 'images/question.jpg' 
+        },
+        comChoiceImg(){
+            return this.comChoice !== null ? `images/${this.comChoice}.jpg` : 'images/question.jpg' 
+        },
+        leftLifeOfMe(){
+            return 3 - this.lifeOfMe
+        },
+        leftLifeOfCom(){
+            return 3 - this.lifeOfCom
+        }
     },
     watch:{
         count(sec){
             if(sec === 0){
-                let number = Math.random()
-                if(number < 0.33){
-                    this.comChoice = 'scissor'
-                }else if(number < 0.66){
-                    this.comChoice = 'rock'
-                }else{
-                    this.comChoice = 'paper'
-                }
+                this.selectCom()
 
-                if(this.myChoice === this.comChoice) this.winner = 'no one'
+                this.whoIsWin()
+
+                this.count = 3
+                this.isSelectable = true
+
+                this.updateLogs()
+                
+            }
+        },
+        lifeOfMe(life){
+            if(life === 0){
+                this.endGame("패배 하였습니다.")
+            }
+        },
+        lifeOfCom(life){
+            if(life === 0){
+                this.endGame("승리 하였습니다.")
+            }
+        }
+    },
+    methods:{
+        startGame(){
+            this.isSelectable = false
+            if(this.myChoice === null){
+                alert('선택한 값이 없습니다.')
+                this.isSelectable = true
+            }else{
+                const countDown = setInterval(() =>{
+                    this.count--
+                    if(this.count === 0 ){
+                        clearInterval(countDown)
+                    }
+                },1000)
+            }
+        },
+        selectCom(){
+            let number = Math.random()
+            if(number < 0.33){
+                this.comChoice = 'scissor'
+            }else if(number < 0.66){
+                this.comChoice = 'rock'
+            }else{
+                this.comChoice = 'paper'
+            }
+        },
+        whoIsWin(){
+            if(this.myChoice === this.comChoice) this.winner = 'no one'
                 else if(this.myChoice === 'rock' && this.comChoice === 'scissor') this.winner = 'me'
                 else if(this.myChoice === 'scissor' && this.comChoice === 'paper') this.winner = 'me'
                 else if(this.myChoice === 'paper' && this.comChoice === 'rock') this.winner = 'me'
@@ -36,55 +94,25 @@ var app = new Vue({
                 }else if(this.winner === 'com'){
                     this.lifeOfMe--
                 }
-                this.count = 3
-                this.isSelectable = true
-                let log = {
-                    message : `You : ${this.myChoice}, Computer: ${this.comChoice}`,
-                    winner : this.winner
-                }
-                this.logs.unshift(log);
-            }
+                
         },
-        lifeOfMe(life){
-            if(life === 0){
-                setTimeout(()=>{
-                    confirm("패배 하였습니다.")
-                    this.lifeOfMe = 3
-                    this.lifeOfCom = 3
-                    this.myChoice = null
-                    this.comChoice = null
-                    this.winner = null
-                    this.logs = []
-                },500)
+        updateLogs(){
+            let log = {
+                message : `You : ${this.myChoice}, Computer: ${this.comChoice}`,
+                winner : this.winner
             }
+            this.logs.unshift(log);
         },
-        lifeOfCom(life){
-            if(life === 0){
-                setTimeout(()=>{
-                    confirm("승리 하였습니다.")
-                    this.lifeOfMe = 3
-                    this.lifeOfCom = 3
-                    this.myChoice = null
-                    this.comChoice = null
-                    this.winner = null
-                    this.logs = []
-                },500)
-            }
-        }
-    },
-    methods:{
-        startGame(){
-            this.isSelectable = false
-            if(this.myChoice === null){
-                alert('선택한 값이 없습니다.')
-            }else{
-                const countDown = setInterval(() =>{
-                    this.count--
-                    if(this.count === 0 ){
-                        clearInterval(countDown)
-                    }
-                },1000)
-            }
+        endGame(message){
+            setTimeout(()=>{
+                confirm(message)
+                this.lifeOfMe = 3
+                this.lifeOfCom = 3
+                this.myChoice = null
+                this.comChoice = null
+                this.winner = null
+                this.logs = []
+            },500)
         }
     }
 })
